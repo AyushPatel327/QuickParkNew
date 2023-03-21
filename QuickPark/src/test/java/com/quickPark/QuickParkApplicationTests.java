a4f0bdc2f4c90630c579c3151c2863b7fd9d172a
AyushPatel327 <100340706+AyushPatel327@users.noreply.github.com>
2023-03-21 12:00:16 +0530

    changes test and port in properties

--- QuickPark/src/test/java/com/quickPark/QuickParkApplicationTests.java
+++ QuickPark/src/test/java/com/quickPark/QuickParkApplicationTests.java
@@ -1,14 +1,10 @@
 package com.quickPark;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class QuickParkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
